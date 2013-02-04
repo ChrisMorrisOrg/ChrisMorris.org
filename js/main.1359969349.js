ad687bd3cf68022969838931ffce4f1f3c142c4b
Chris <chris@chrismorris.org>
2013-02-04 20:19:06 +1100

    v1.2.71
    Moved Facebook, Twitter, and Google Analytics plugins into main.js file.

--- js/main.1359969349.js
+++ js/main.1359969349.js
@@ -56,3 +56,29 @@ function resizeSidebar(){
 }
 
 window.onbeforeunload = function(){document.getElementById("container").className = "inactive";};
+
+
+<!-- Plugins -->
+  <!-- Google Analytics -->
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36771574-1']);
+    _gaq.push(['_setDomainName', 'chrismorris.org']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  <!-- Facebook -->
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=1721669711307072";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  <!-- Twitter -->
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
