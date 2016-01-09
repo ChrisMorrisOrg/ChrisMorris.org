5da4559f2a41acbbbcd22becd6e3c6b8a77fa86d
Chris Morris <chris@chrismorris.org>
2016-01-09 22:58:27 +1100

    insert fix

--- _server/chrismorris.org.js
+++ _server/chrismorris.org.js
@@ -24,16 +24,17 @@ app.post('/views', function(req, res){
   connection.query('SELECT FORMAT(views+1, 0) AS views FROM views WHERE url = ?', [slug], function(err, rows, fields) {
     if (err) {
       console.error(err.message)
-    } else if (rows.affectedRows === 0) {
-      // Insert a new record if none exists
-      connection.query('INSERT INTO views (slug, views) VALUES (?, ?)', [slug, 0], function (err, rows) {
-        if (err)
-          console.error(err.message)
-        else if (rows.affectedRows !== 1)
-          console.error('ERROR: Inserting new slug failed')
-        else
-          console.error('Added new slug ' + slug)
-      });
+      if (rows.affectedRows === 0) {
+        // Insert a new record if none exists
+        connection.query('INSERT INTO views (slug, views) VALUES (?, ?)', [slug, 0], function (err, rows) {
+          if (err)
+            console.error(err.message)
+          else if (rows.affectedRows !== 1)
+            console.error('ERROR: Inserting new slug failed')
+          else
+            console.error('Added new slug ' + slug)
+        });
+      }
     }
 
     console.log("POST: View incremented on slug = '" + slug + "', views now = " + rows[0]['views']);
