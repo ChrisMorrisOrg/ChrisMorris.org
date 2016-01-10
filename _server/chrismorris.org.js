cb6ef89a791ada970b66cebe5ebd6c6616a47111
Chris Morris <chris@chrismorris.org>
2016-01-10 12:42:00 +1100

    Implemented standard

--- _server/chrismorris.org.js
+++ _server/chrismorris.org.js
@@ -1,24 +1,23 @@
-var express	= require('express');
-var bodyParser = require('body-parser');
-var app	= express();
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 var port = process.argv[2]
 
 if (!port) {
   port = 3000
 }
-console.log ('Using port ' + port)
+console.log('Using port ' + port)
 
-app.post('/views', function(req, res){
+app.post('/views', function (req, res) {
+  var url = (req.body.slug).replace(/^\/|\/$/g, '') // Strip leading and trailing slashes
+  var mysql = require('mysql')
+  var mysqlConfig = require('./mysql-config')
+  var connection = mysql.createConnection(mysqlConfig.db)
 
-  var url		= (req.body.slug).replace(/^\/|\/$/g, ''); // Strip leading and trailing slashes
-  var mysql		= require('mysql');
-  var mysqlConfig = require('./mysql-config');
-  var connection	= mysql.createConnection(mysqlConfig.db);
-
-  connection.connect();
+  connection.connect()
 
   // Get view count and send it
   connection.query('SELECT views FROM views WHERE url = ?', [url], function (err, results) {
@@ -33,32 +32,29 @@ app.post('/views', function(req, res){
 
     // Asyncronously update the view count
     connection.query(
-        'UPDATE views SET views=views+1 WHERE url = ?', [url],
-        function (err, results) {
-      if (err) {
-        console.error(err.message)
-        connection.end()
-      } else if (results.affectedRows === 0) {
-        // If no rows were affected, then this is a new post, so add it
-        connection.query(
+      'UPDATE views SET views=views+1 WHERE url = ?', [url],
+      function (err, results) {
+        if (err) {
+          console.error(err.message)
+          connection.end()
+        } else if (results.affectedRows === 0) {
+          // If no rows were affected, then this is a new post, so add it
+          connection.query(
             'INSERT INTO views (url, views) VALUES (?, ?)', [url, 1],
             function (err, results) {
-          if (err)
-            console.error(err.message)
-          else if (results.affectedRows !== 1)
-            console.error('ERROR: Inserting new url failed')
-          else
-            console.error('Added new url ' + url)
-
+              if (err) {
+                console.error(err.message)
+              } else if (results.affectedRows !== 1) {
+                console.error('ERROR: Inserting new url failed')
+              } else {
+                console.error('Added new url ' + url)
+              }
+              connection.end()
+            })
+        } else {
           connection.end()
-        })
-      } else {
-        connection.end()
-      }
-    })
+        }
+      })
   })
-
-});
-
-
-app.listen(port);
+})
+app.listen(port)
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Shamelessly taken from @feross
-function addCommasToInteger(x) {
+function addCommasToInteger (x) {
   var rgx = /(\d+)(\d{3})/
   x += ''
 
@@ -9,12 +9,12 @@ function addCommasToInteger(x) {
   return x
 }
 
-$(document).ready(function(){
-  $.ajaxSetup({cache:false});
-  var slug;
-  slug = $('.views').data('slug');
+$(document).ready(function () {
+  $.ajaxSetup({cache: false})
+  var slug
+  slug = $('.views').data('slug')
 
-  if(slug){
+  if (slug) {
     $.ajax({
       type: 'POST',
       url: '/views',
@@ -22,62 +22,60 @@ $(document).ready(function(){
         slug: slug
       },
       dataType: 'json',
-      success: function(data) {
-        $('.views').text(addCommasToInteger(data.views) + " views");
-        $('.views').removeClass('inactive').addClass('active');
+      success: function (data) {
+        $('.views').text(addCommasToInteger(data.views) + ' views')
+        $('.views').removeClass('inactive').addClass('active')
       },
-      error: function(data, textStatus, errorThrown) {
-      }
-    });
+      error: function (data, textStatus, errorThrown) {}
+    })
   }
 
-  function getQuote(){
+  function getQuote () {
+    var quotes = [{'author': 'Albert Einstein', 'quote': 'Ultimate automation … will make our modern industry as primitive and outdated as the stone age man looks to us today.'},
+      {'author': 'K. Eric Drexler', 'quote': 'In thinking about nanotechnology today, what’s most important is understanding where it leads, what nanotechnology will look like after we reach the assembler breakthrough.'},
+      {'author': 'Dr. Michio Kaku', 'quote': 'In fact, if appearance and essence were the same thing, there would be no need for science.'},
+      {'author': 'Edward R. Harrison', 'quote': 'Hydrogen is a light, odorless gas, which, given enough time, turns into people.'},
+      {'author': 'Ken Segall', 'quote': 'In the world of Apple, a Pro product used to mean “designed for high-end professionals with needs far beyond those of mortal men.” Now it simply means “the high-performance model.”'},
+      {'author': 'Alan Kay', 'quote': 'People who are really serious about software should make their own hardware.'},
+      {'author': 'Albert Einstein', 'quote': 'Logic will get you from A to B. Imagination will take you everywhere.'},
+      {'author': 'Richard P. Feynman', 'quote': 'Physics is like sex. Sure, it may give some practical results, but that’s not why we do it.'},
+      {'author': 'Abraham Lincoln', 'quote': 'I destroy my enemies when I make them my friends.'},
+      {'author': 'Ambrose Moon', 'quote': 'Courage is not the absence of fear, but rather the judgement that something else is more important than fear.'},
+      {'author': 'Kenneth Boulding', 'quote': 'Anyone who believes exponential growth can go on forever in a finite world is either a madman or an economist.'},
+      {'author': 'Richard Dawkins', 'quote': 'It has become almost a cliché to remark that nobody boasts of ignorance of literature, but it is socially acceptable to boast ignorance of science and proudly claim incompetence in mathematics.'},
+      {'author': 'William Gibson', 'quote': 'Before you diagnose yourself with depression or low self-esteem, first make sure that you are not, in fact, just surrounded by assholes.'},
+      {'author': 'Dwight Schrute', 'quote': 'Would I ever leave this company? Look, I’m all about loyalty. In fact, I feel like part of what I’m being paid for here is my loyalty. But if there were somewhere else that valued loyalty more highly, I’m going wherever they value loyalty the most.'},
+      {'author': 'Neil deGrasse Tyson', 'quote': 'Actually, America is inching its way towards the metric system.'},
+      {'author': 'Stephen Colbert', 'quote': 'I tweet, therefore I am.'},
+      {'author': 'Karl Von Clausewitz', 'quote': 'War is a continuation of politics by other means. Politics is a continuation of economics by other means.'},
+      {'author': 'Thomas Jefferson', 'quote': 'When the people fear the government there is tyranny, when the government fears the people there is liberty.'},
+      {'author': 'Ayn Rand', 'quote': 'A creative man is motivated by the desire to achieve, not by the desire to beat others.'},
+      {'author': 'Winston Churchill', 'quote': "If you're going through hell, keep going."},
+      {'author': 'Og Mandino', 'quote': 'Always do your best. What you plant now, you will harvest later.'},
+      {'author': 'Walt Disney', 'quote': 'If you can dream it, you can do it.'},
+      {'author': 'Thomas Edison', 'quote': 'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.'},
+      {'author': 'Ken Venturi', 'quote': "I don't believe you have to be better than everybody else. I believe you have to be better than you ever thought you could be."},
+      {'author': 'Albert Einstein', 'quote': "When you are courting a nice girl an hour seems like a second. When you sit on a red-hot cinder a second seems like an hour. That's relativity."},
+      {'author': 'Elbert Hubbard', 'quote': 'Do not take life too seriously. You will never get out of it alive.'},
+      {'author': 'Isaac Asimov', 'quote': 'People who think they know everything are a great annoyance to those of us who do.'},
+      {'author': 'Helen Keller', 'quote': 'Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.'},
+      {'author': 'Theodore Roosevelt', 'quote': 'Far better is it to dare mighty things, to win glorius triumphs, even though checkered by failure... than to rank with those poor spirits who neither enjoy nor suffer much, because they live in a gray twilight that knows not victory nor defeat.'},
+      {'author': 'Roy H. Williams', 'quote': 'A smart man makes a mistake, learns from it, and never makes that mistake again. But a wise man finds a smart man and learns from him how to avoid the mistake altogether.'},
+      {'author': 'Albert Einstein', 'quote': 'The difference between stupidity and genius is that genius has its limits.'},
+      {'author': 'Steve Jobs', 'quote': 'Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma - which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.'},
+      {'author': 'Arthur C. Clarke', 'quote': 'Any sufficiently advanced technology is indistinguishable from magic.'}
+    ]
+    var chosenQuote = quotes[Math.floor(Math.random() * quotes.length)]
 
-    var quotes = [{"author":"Albert Einstein", "quote":"Ultimate automation … will make our modern industry as primitive and outdated as the stone age man looks to us today."},
-                  {"author":"K. Eric Drexler", "quote":"In thinking about nanotechnology today, what’s most important is understanding where it leads, what nanotechnology will look like after we reach the assembler breakthrough."},
-                  {"author":"Dr. Michio Kaku", "quote":"In fact, if appearance and essence were the same thing, there would be no need for science."},
-                  {"author":"Edward R. Harrison", "quote":"Hydrogen is a light, odorless gas, which, given enough time, turns into people."},
-                  {"author":"Ken Segall", "quote":"In the world of Apple, a Pro product used to mean “designed for high-end professionals with needs far beyond those of mortal men.” Now it simply means “the high-performance model.”"},
-                  {"author":"Alan Kay", "quote":"People who are really serious about software should make their own hardware."},
-                  {"author":"Albert Einstein", "quote":"Logic will get you from A to B. Imagination will take you everywhere."},
-                  {"author":"Richard P. Feynman", "quote":"Physics is like sex. Sure, it may give some practical results, but that’s not why we do it."},
-                  {"author":"Abraham Lincoln", "quote":"I destroy my enemies when I make them my friends."},
-                  {"author":"Ambrose Moon", "quote":"Courage is not the absence of fear, but rather the judgement that something else is more important than fear."},
-                  {"author":"Kenneth Boulding", "quote":"Anyone who believes exponential growth can go on forever in a finite world is either a madman or an economist."},
-                  {"author":"Richard Dawkins", "quote":"It has become almost a cliché to remark that nobody boasts of ignorance of literature, but it is socially acceptable to boast ignorance of science and proudly claim incompetence in mathematics."},
-                  {"author":"William Gibson", "quote":"Before you diagnose yourself with depression or low self-esteem, first make sure that you are not, in fact, just surrounded by assholes."},
-                  {"author":"Dwight Schrute", "quote":"Would I ever leave this company? Look, I’m all about loyalty. In fact, I feel like part of what I’m being paid for here is my loyalty. But if there were somewhere else that valued loyalty more highly, I’m going wherever they value loyalty the most."},
-                  {"author":"Neil deGrasse Tyson", "quote":"Actually, America is inching its way towards the metric system."},
-                  {"author":"Stephen Colbert", "quote":"I tweet, therefore I am."},
-                  {"author":"Karl Von Clausewitz", "quote":"War is a continuation of politics by other means. Politics is a continuation of economics by other means."},
-                  {"author":"Thomas Jefferson", "quote":"When the people fear the government there is tyranny, when the government fears the people there is liberty."},
-                  {"author":"Ayn Rand", "quote":"A creative man is motivated by the desire to achieve, not by the desire to beat others."},
-                  {"author":"Winston Churchill", "quote":"If you\'re going through hell, keep going."},
-                  {"author":"Og Mandino", "quote":"Always do your best. What you plant now, you will harvest later."},
-                  {"author":"Walt Disney", "quote":"If you can dream it, you can do it."},
-                  {"author":"Thomas Edison", "quote":"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time."},
-                  {"author":"Ken Venturi", "quote":"I don\'t believe you have to be better than everybody else. I believe you have to be better than you ever thought you could be."},
-                  {"author":"Albert Einstein", "quote":"When you are courting a nice girl an hour seems like a second. When you sit on a red-hot cinder a second seems like an hour. That\'s relativity."},
-                  {"author":"Elbert Hubbard", "quote":"Do not take life too seriously. You will never get out of it alive."},
-                  {"author":"Isaac Asimov", "quote":"People who think they know everything are a great annoyance to those of us who do."},
-                  {"author":"Helen Keller", "quote":"Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence."},
-                  {"author":"Theodore Roosevelt", "quote":"Far better is it to dare mighty things, to win glorius triumphs, even though checkered by failure... than to rank with those poor spirits who neither enjoy nor suffer much, because they live in a gray twilight that knows not victory nor defeat."},
-                  {"author":"Roy H. Williams", "quote":"A smart man makes a mistake, learns from it, and never makes that mistake again. But a wise man finds a smart man and learns from him how to avoid the mistake altogether."},
-                  {"author":"Albert Einstein", "quote":"The difference between stupidity and genius is that genius has its limits."},
-                  {"author":"Steve Jobs", "quote":"Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma - which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition."},
-                  {"author":"Arthur C. Clarke", "quote":"Any sufficiently advanced technology is indistinguishable from magic."}
-                ];
-    var chosenQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    $('.box').html('“' + chosenQuote.quote + '”' + '<span class="quoteAuthor">~ ' + chosenQuote.author + ' ~</span>')
+  }
 
-    $('.box').html("“" + chosenQuote.quote + "”" + "<span class=\"quoteAuthor\">~ " + chosenQuote.author + " ~</span>");
-  };
+  $('#getQuote').click(function () {
+    getQuote()
+  })
 
-  $('#getQuote').click(function(){
-    getQuote();
-  });
-
-  getQuote();
-});
+  getQuote()
+})
 
 if (typeof lightbox !== 'undefined') {
   lightbox.option({
